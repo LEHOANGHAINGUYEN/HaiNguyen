6076504f4ded41861295ba994309c7efcfc24f07
HaiNguyen <lehoanghainguyen96@gmail.com>
2016-09-26 08:35:06 +0700

    baitap1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args);
+
+        class Element<T>
+        {
+            private T data;
+
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+
+
+            Element<T> next;
+
+            public Element<T> Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+
+
+        }
+    }
+}
