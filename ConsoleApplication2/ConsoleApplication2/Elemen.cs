93f8a6cd77d0c3454bf6c05106b191a3d6014bb5
HaiNguyen <lehoanghainguyen96@gmail.com>
2016-09-26 08:51:55 +0700

    th

---
+++ ConsoleApplication2/ConsoleApplication2/Elemen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Elemen<T>
+    {
+        private T data;
+        private T Data;
+    
+        
+
+public T Data1
+{
+  get { return Data; }
+  set { Data = value; }
+}
+        Elemen<T> next;
+
+    internal Elemen<T> Next
+   {
+  get { return next; }
+  set { next = value; }
+}
+       
+    }
+}
