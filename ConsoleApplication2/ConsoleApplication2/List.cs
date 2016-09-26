478717fc05ebfb74d8d51461f99e91cb54005261
HaiNguyen <lehoanghainguyen96@gmail.com>
2016-09-26 09:12:16 +0700

    thuchanh1

---
+++ ConsoleApplication2/ConsoleApplication2/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class List<T>
+    {
+        Elemen<T> head;
+
+internal Elemen<T> Head
+{
+  get { return head; }
+  set { head = value; }
+}
+public List()
+{}
+    public void addfirst(Elemen<T> e){
+        e.Next=head;
+        this.head=e;
+    }
+}
+
+}
